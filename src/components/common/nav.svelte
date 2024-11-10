c7b82f49c358811c184bb2a80ec273d0dc0aa737
95hanho <hosung6674@naver.com>
2024-11-10 23:01:54 +0900

    유저 관련 api부터 만들자!

--- src/components/common/nav.svelte
+++ src/components/common/nav.svelte
@@ -55,7 +55,6 @@
             {/if}
         </div>
     </div>
-    
 </nav>
 <!-- END navInfo -->
 
--- src/router.js
+++ src/router.js
@@ -26,181 +26,182 @@ import { modal_alert } from "./store/modalSlice";
 
 // 로그인 페이지에 토큰체크 & 헤더네임가져오기
 const loginUrlCheck = async () => {
-	await loginOn.tokenCheck();
+  await loginOn.tokenCheck();
 };
 // 관리자 권한 검사
 const adminCheck = async () => {
-	let result = false;
-	await getUser().then((userInfo) => {
-		result = userInfo.grant;
-		if (result != 90) {
-			modal_alert.open("관리자 권한이 필요합니다.");
-			replace("/");
-		}
-	});
-	return result;
+  let result = false;
+  await getUser().then((userInfo) => {
+    console.log(userInfo);
+    result = userInfo.grant;
+    if (result != 90) {
+      modal_alert.open("관리자 권한이 필요합니다.");
+      replace("/");
+    }
+  });
+  return result;
 };
 // 운영자 권한 검사
 const managerCheck = async () => {
-	let result = false;
-	await getUser().then((userInfo) => {
-		result = userInfo.grant;
-		if (result != 80) {
-			modal_alert.open("운영자 권한이 필요합니다.");
-			replace("/");
-		}
-	});
-	return result;
+  let result = false;
+  await getUser().then((userInfo) => {
+    result = userInfo.grant;
+    if (result != 80) {
+      modal_alert.open("운영자 권한이 필요합니다.");
+      replace("/");
+    }
+  });
+  return result;
 };
 const talktalk = {
-	"/talktalk/write/:talkId": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return TalktalkModify;
-		},
-	}),
-	"/talktalk/write": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return TalktalkAdd;
-		},
-	}),
-	"/talktalk/:pageNum/:talkId": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return TalktalkDetail;
-		},
-	}),
-	"/talktalk/:pageNum": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return Talktalk;
-		},
-	}),
+  "/talktalk/write/:talkId": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return TalktalkModify;
+    }
+  }),
+  "/talktalk/write": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return TalktalkAdd;
+    }
+  }),
+  "/talktalk/:pageNum/:talkId": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return TalktalkDetail;
+    }
+  }),
+  "/talktalk/:pageNum": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return Talktalk;
+    }
+  })
 };
 const notice = {
-	"/notice/notice-write/:noticeId": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await adminCheck())) return;
-			return NoticeModify;
-		},
-	}),
-	"/notice/notice-write": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await adminCheck())) return;
-			return NoticeAdd;
-		},
-	}),
-	"/notice/write/:qnaId": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return NoticeQnaModify;
-		},
-	}),
-	"/notice/write": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return NoticeQnaAdd;
-		},
-	}),
-	"/notice/:pageNum": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return Notice;
-		},
-	}),
-	"/notice/:pageNum/:noticeId": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return NoticeDetail;
-		},
-	}),
+  "/notice/notice-write/:noticeId": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await adminCheck())) return;
+      return NoticeModify;
+    }
+  }),
+  "/notice/notice-write": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await adminCheck())) return;
+      return NoticeAdd;
+    }
+  }),
+  "/notice/write/:qnaId": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return NoticeQnaModify;
+    }
+  }),
+  "/notice/write": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return NoticeQnaAdd;
+    }
+  }),
+  "/notice/:pageNum": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return Notice;
+    }
+  }),
+  "/notice/:pageNum/:noticeId": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return NoticeDetail;
+    }
+  })
 };
 
 const router = {
-	"/": wrap({
-		asyncComponent: async () => {
-			loginOn.subscribe((value) => {
-				if (value) loginUrlCheck();
-			});
-			return Main;
-		},
-	}),
-	/*  */
-	"/program": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return Program;
-		},
-	}),
-	"/program/:pNum": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return ProgramDetail;
-		},
-	}),
-	/*  */
-	"/application/:viewForm": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			return Application;
-		},
-	}),
-	/*  */
-	...talktalk,
-	/*  */
-	...notice,
-	/*  */
-	"/admin/program/add": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await adminCheck())) return;
-			return AdminProgramAdd;
-		},
-	}),
-	"/admin/program/:pNum": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await adminCheck())) return;
-			return AdminProgramModify;
-		},
-	}),
-	"/admin/:menu": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await adminCheck())) return;
-			return Admin;
-		},
-	}),
-	/*  */
-	"/manager": wrap({
-		asyncComponent: async () => {
-			await loginUrlCheck();
-			if (!(await managerCheck())) return;
-			return Manager;
-		},
-	}),
-	// 개발중 페이지
-	"/ready": wrap({
-		asyncComponent: async () => {
-			return Ready;
-		},
-	}),
-	// 없는 페이지
-	"/notPage": wrap({
-		asyncComponent: async () => {
-			return NotPage;
-		},
-	}),
-	// 12321
-	"/*": wrap({
-		asyncComponent: () => {
-			replace("/notPage");
-			return null;
-		},
-	}),
+  "/": wrap({
+    asyncComponent: async () => {
+      loginOn.subscribe((value) => {
+        if (value) loginUrlCheck();
+      });
+      return Main;
+    }
+  }),
+  /*  */
+  "/program": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return Program;
+    }
+  }),
+  "/program/:pNum": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return ProgramDetail;
+    }
+  }),
+  /*  */
+  "/application/:viewForm": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      return Application;
+    }
+  }),
+  /*  */
+  ...talktalk,
+  /*  */
+  ...notice,
+  /*  */
+  "/admin/program/add": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await adminCheck())) return;
+      return AdminProgramAdd;
+    }
+  }),
+  "/admin/program/:pNum": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await adminCheck())) return;
+      return AdminProgramModify;
+    }
+  }),
+  "/admin/:menu": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await adminCheck())) return;
+      return Admin;
+    }
+  }),
+  /*  */
+  "/manager": wrap({
+    asyncComponent: async () => {
+      await loginUrlCheck();
+      if (!(await managerCheck())) return;
+      return Manager;
+    }
+  }),
+  // 개발중 페이지
+  "/ready": wrap({
+    asyncComponent: async () => {
+      return Ready;
+    }
+  }),
+  // 없는 페이지
+  "/notPage": wrap({
+    asyncComponent: async () => {
+      return NotPage;
+    }
+  }),
+  // 12321
+  "/*": wrap({
+    asyncComponent: () => {
+      replace("/notPage");
+      return null;
+    }
+  })
 };
 
 export default router;
